1d989faa7b14c7049f66b0b1a7e0ec9a655c2472
deekshithkumar <54591741+deekshith-k@users.noreply.github.com>
2020-10-15 14:51:34 +0530

    vehicle showroom

---
+++ dbconnection.php
@@ -0,0 +1,7 @@
+<?php
+$con=mysqli_connect("localhost","root","technology","vehicleshowroom");
+if(!$con)
+{
+	die("could not connect".mysqli_error());
+}
+?>
---
+++ footer.php
@@ -0,0 +1,18 @@
+    <div id="templatemo_footer">
+    	<p><a href="index.php">Home</a> | 
+        <a href="vehicles.php">Vehicles</a> | 
+        <a href="about.php">About</a> | 
+         <a href="contact.php">Contact Us</a>
+		</p>
+
+    	Copyright © 2013-2014 | Developed by <a href="http://www.freestudentprojects.com">www.freestudentprojects.com</a>
+ 
+    </div> <!-- END of templatemo_footer -->
+    
+</div> <!-- END of templatemo_wrapper -->
+</div> <!-- END of templatemo_body_wrapper -->
+
+
+<script type='text/javascript' src='js/logging.js'></script>
+</body>
+</html>
